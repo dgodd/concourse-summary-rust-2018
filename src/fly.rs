e174ce22ede3f55970c4f0e255bb1c0fd7c023c3
Dave Goddard <dave@goddard.id.au>
2018-10-11 17:24:38 -0300

    download all data from all hosts in flyrc

---
+++ src/fly.rs
@@ -0,0 +1,72 @@
+use std::{
+    collections::BTreeMap,
+    path::Path,
+    env,
+    fs::File,
+    io::prelude::Read,
+    error::Error,
+};
+
+#[derive(Debug, Deserialize)]
+struct Token {
+    value: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Target {
+    api: String,
+    token: Token,
+}
+
+#[derive(Debug, Deserialize)]
+struct FlyRC {
+    targets: BTreeMap<String, Target>,
+}
+
+pub fn get_rc() -> Result<Vec<(String, String, String)>, Box<dyn Error>> {
+    let home = env::var("HOME")?;
+    let mut f = File::open(Path::new(&home).join(".flyrc"))?;
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+    let fly_rc: FlyRC = serde_yaml::from_str(&contents)?;
+    let targets = fly_rc.targets.iter().map(|(key,target)| (key.to_owned(),target.api.to_owned(), target.token.value.to_owned())).collect();
+    Ok(targets)
+}
+
+#[derive(Debug, Clone)]
+pub struct Pipeline {
+    name: String,
+    num_jobs: u64,
+    statuses: BTreeMap<String, u64>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Job {
+    pipeline_name: String,
+    // team_name: String,
+    finished_build: Option<Build>,
+}
+#[derive(Debug, Deserialize)]
+struct Build {
+    status: String,
+}
+
+pub fn get_pipelines(host: &str) -> Result<Vec<Pipeline>, Box<dyn Error>> {
+    let url = format!("{}/api/v1/jobs", host);
+    let json: Vec<Job> = reqwest::get(&url)?.json()?;
+
+    let mut a = BTreeMap::new();
+    for ref job in &json {
+        let pipeline = a.entry(job.pipeline_name.to_owned()).or_insert(Pipeline{name:job.pipeline_name.to_owned(),num_jobs:0,statuses: BTreeMap::new()});
+        pipeline.num_jobs += 1;
+        match &job.finished_build {
+            Some(fb) => {
+                let status = &fb.status;
+                *pipeline.statuses.entry(status.to_owned()).or_insert(0) += 1;
+            },
+            None => {},
+        }
+    }
+
+    Ok(a.values().cloned().collect())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+#[macro_use] extern crate serde_derive;
 use std::{
     error::Error,
 };
+mod fly;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // http://ci.pcfdev.io/api/v1/info ;; pipelines ;; resources ;; userinfor ;; jobs
 
-    let url = format!("http://ci.pcfdev.io/api/v1/jobs");
-    let json: serde_json::Value = reqwest::get(&url)?.json()?;
-
-    println!("JSON: {:?}", json);
+    let targets = fly::get_rc()?;
+    for (name, host, _token) in &targets {
+        println!("-----> {}", name);
+        let pipelines = fly::get_pipelines(host)?;
+        for pipeline in &pipelines {
+            println!("  PIPELINE: {:?}", pipeline);
+        }
+    }
 
     Ok(())
 }
