f330e93bad067f83ee6b58eadd2fed39c29ca626
Dave Goddard <dave@goddard.id.au>
2018-10-11 21:45:49 -0300

    color paused pipelines

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,21 @@ fn main() -> Result<(), Box<dyn Error>> {
         // if name != "ci" {
         //     continue;
         // }
-        println!("{} {}", "-->".red(), name.blue().bold());
+        println!("{} {}", "-->".red(), name.bold());
         let pipelines = fly::get_pipelines(host, token)?;
         for pipeline in &pipelines {
-            print!("    {}:", pipeline.name.bold());
+            let mut pipeline_name = pipeline.name.normal();
+            if pipeline.paused {
+                pipeline_name = pipeline_name.bold().blue();
+            }
+            print!("    {}:", pipeline_name);
             for (key, val) in &pipeline.statuses {
                 let perc = format!("{}%", 100 * val / pipeline.num_jobs);
                 print!(
                     " {}",
                     match key {
                         fly::Status::Success => perc.green(),
-                        fly::Status::Fail => perc.red(),
+                        fly::Status::Fail => perc.red().bold(),
                         _ => format!("{:?}:{}%", key, perc).yellow(),
                     }
                 )
